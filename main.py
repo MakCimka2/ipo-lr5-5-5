957da3f3a54b39d65fc4ad5fffe81bfeef9de566
MakCimka <poduskapuska@gmail.com>
2024-11-21 10:31:57 +0300

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ a ="Каша Ложка Python Python"
 count = 0
 b = a.split()
 for i in b:
-    if i == "Python":
-        count += 1
+    if i == "Python": #Считаем сколько раз в списке встречается слово питон
+        count += 1  #Прибавляем к счетчику 1
 print(count)
